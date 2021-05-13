2ac3419db1d7a6b746ad90b38e39803eca2ebf1b
rikako-yurino <dawnpurple.1989@gmail.com>
2021-05-13 14:01:22 +0900

    commit

---
+++ base_python/base4.py
@@ -0,0 +1,37 @@
+#数値型
+
+value = 1
+# print(value)
+# value = -2
+# print(value)
+# value += 4
+# print(value)
+# print(value * 4)
+# print(value / 3)
+# value = 10
+# print(value // 3)
+# print(value %3)
+
+value += 3
+# value -= 2
+# print(value)
+# print(value ** 3)
+
+#浮動小数点数
+# height = 175.5
+# print(height)
+# print(type(height))
+# print(height + 10)
+
+# value = 8
+# print(value >> 2)
+# print(value << 3)
+
+# print(12 & 21)
+# print(12 | 21)
+
+value = 12
+value &= 21
+print(value)
+value |= 12
+print(value)
