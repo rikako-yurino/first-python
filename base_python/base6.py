8ae6c44099c081189f06e74b7618db677fcf6bb8
rikako-yurino <dawnpurple.1989@gmail.com>
2021-05-14 13:40:53 +0900

    commit

---
+++ base_python/base6.py
@@ -0,0 +1,18 @@
+#複素数
+# a = 1 + 3j
+# b = 3+ 5j
+# print(a,b)
+# print(a + b)
+# print(a - b)
+# print(a*b)
+
+#complex
+a = complex(1,3)
+b = complex(3,5)
+print(a,b)
+print(a + b)
+print(a - b)
+print(a*b)
+
+print(a.real)
+print(a.imag)
