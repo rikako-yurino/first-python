07d784511dde5082ab58e5773c3d3561360b0b56
rikako-yurino <dawnpurple.1989@gmail.com>
2021-05-17 10:20:09 +0900

    commit

--- base_python/base9.py
+++ base_python/base9.py
@@ -12,3 +12,24 @@ print(list_a[1][2])
 print(list_a)
 list_a[1][2] = 'lemon'
 print(list_a)
+
+#リストのメソッド
+list_a = [1,2,3,4,5]
+
+list_b = list_a[1:4:2]
+print(list_b)
+
+#append, extend, insert, clear
+list_a.append('apple')
+print(list_a)
+list_a.extend(['banana', 'melon'])
+print(list_a)
+
+list_a.insert(1,'grape')
+print(list_a)
+
+# list_a.clear()
+# print(list_a)
+
+
+
